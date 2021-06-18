e4d162e7a17b23a5c6028010ef5abde51b3ee314
Mark Goddard <mark@stackhpc.com>
2021-06-18 11:26:58 +0100

    Support configuration of firewalld
    Adds support for configuring firewalld for CentOS hosts managed by
    Kayobe.
    
    * create zones
    * set default zone
    * set zone for interfaces
    * define rules
    
    Change-Id: Id60e25e129e323f3c07e702bb81a11efc530fb3e
    Story: 2008991
    Task: 42644
    (cherry picked from commit 7d15aa16f22a1a413ca4be09fb7f32cf1244cf4a)

--- kayobe/tests/unit/cli/test_commands.py
+++ kayobe/tests/unit/cli/test_commands.py
@@ -327,6 +327,7 @@ class TestCase(unittest.TestCase):
                     utils.get_data_files_path("ansible", "users.yml"),
                     utils.get_data_files_path("ansible", "dev-tools.yml"),
                     utils.get_data_files_path("ansible", "network.yml"),
+                    utils.get_data_files_path("ansible", "firewall.yml"),
                     utils.get_data_files_path("ansible", "sysctl.yml"),
                     utils.get_data_files_path("ansible", "time.yml"),
                     utils.get_data_files_path("ansible", "mdadm.yml"),
@@ -500,6 +501,7 @@ class TestCase(unittest.TestCase):
                     utils.get_data_files_path(
                         "ansible", "disable-selinux.yml"),
                     utils.get_data_files_path("ansible", "network.yml"),
+                    utils.get_data_files_path("ansible", "firewall.yml"),
                     utils.get_data_files_path("ansible", "sysctl.yml"),
                     utils.get_data_files_path("ansible", "ip-routing.yml"),
                     utils.get_data_files_path("ansible", "snat.yml"),
@@ -1266,6 +1268,7 @@ class TestCase(unittest.TestCase):
                     utils.get_data_files_path(
                         "ansible", "disable-selinux.yml"),
                     utils.get_data_files_path("ansible", "network.yml"),
+                    utils.get_data_files_path("ansible", "firewall.yml"),
                     utils.get_data_files_path("ansible", "sysctl.yml"),
                     utils.get_data_files_path("ansible", "disable-glean.yml"),
                     utils.get_data_files_path(
